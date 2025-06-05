a5f41949040c459cf57b716f3593310f758d933f
mentalhetal <37432702+mentalhetal@users.noreply.github.com>
2025-06-05 16:54:33 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-export AWS_REGION="ap-northeast-2"
-export ECR_REPO="tracking-app"
-export ECR_URI="122996776662.dkr.ecr.$AWS_REGION.amazonaws.com"
+ACCOUNT_ID="122996776662"  # ✅ 본인 AWS 계정 ID 입력
+ECR_URI="$ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com"
+REPO="tracking-app"
 
 echo "[1] Docker Build"
-docker build -t $ECR_REPO ./app
-docker tag $ECR_REPO:latest $ECR_URI/$ECR_REPO:latest
+docker build -t $REPO ./app
+docker tag $REPO:latest $ECR_URI/$REPO:latest
 
-echo "[2] ECR Push"
-aws ecr get-login-password --region $AWS_REGION | \
+echo "[2] Docker Push"
+aws ecr get-login-password --region ap-northeast-2 | \
   docker login --username AWS --password-stdin $ECR_URI
-docker push $ECR_URI/$ECR_REPO:latest
+docker push $ECR_URI/$REPO:latest
 
 echo "[3] EC2 인벤토리 생성"
 aws ec2 describe-instances \
@@ -25,5 +25,5 @@ while read ip; do
   echo "$ip ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/seoul.key.pem" >> ansible/inventory.ini
 done < ips.txt
 
-echo "[4] Ansible 배포"
+echo "[4] Ansible 실행"
 ansible-playbook -i ansible/inventory.ini ansible/playbook.yml
